9059815f83483078a40f205814beaa3bdd182be7
jaylenchen <jaylen-work@outlook.com>
2025-06-24 08:38:34 +0800

    feat: top-100-liked

--- packages/leetcode/top-100-liked/src/common/binary-tree/diameter-of-binary-tree.ts
+++ packages/leetcode/top-100-liked/src/common/binary-tree/diameter-of-binary-tree.ts
@@ -23,16 +23,29 @@ import { TreeNode } from "./types";
 
 function diameterOfBinaryTree(root: TreeNode | null): number {
   let ans = 0;
-
+  // 链：从子树中的叶子节点到当前节点的路径。
+  // 整体思路是：遍历每一个节点，然后在每一层计算当前子树的最大链长，然后加上1，形成当前子树的左侧链长和右侧链长，然后更新最大值。
+  // dfs表示寻找当前子树的最大链长，返回值为当前子树的最大链长，不包含当前节点和其父节点之间的这条边。
+  // 比如一棵树长这样：
+  //                 1
+  //   dfs(l)+1     / \    dfs(r)+1
+  //               2   3
+  //              / \   \
+  //             4   5   6
+  // 对于：
+  // 1-2-4 这条链的边长为 2，相当于2-4这条链的边长+1
+  // 2-4 这条链的边长为 1
+  // 从4出发，边长为0
+  // 从空节点出发，边长为-1
   function dfs(node: TreeNode | null): number {
     if (node === null) {
-      return -1; // 对于叶子来说，链长就是 -1+1=0
+      return -1; 
     }
 
-    const lLen = dfs(node.left) + 1; // 左子树最大链长+1
-    const rLen = dfs(node.right) + 1; // 右子树最大链长+1
+    const lLen = dfs(node.left) + 1; // 左子树最大链长+1，就是加上左子树到当前节点的边
+    const rLen = dfs(node.right) + 1; // 右子树最大链长+1，就是加上右子树到当前节点的边
 
-    ans = Math.max(ans, lLen + rLen); // 两条链拼成路径
+    ans = Math.max(ans, lLen + rLen); // 两条链拼成路径，更新最大值
 
     return Math.max(lLen, rLen); // 当前子树最大链长
   }
